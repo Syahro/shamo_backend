dc78f99dc8c267c2bff6f5652e15cf333cefa6df
Arief Syahroni <riefsyah31@gmail.com>
2021-09-02 09:21:18 +0700

    add new api for get checkout data and add route for access an api

--- app/Models/TransactionItem.php
+++ app/Models/TransactionItem.php
@@ -10,7 +10,7 @@ class TransactionItem extends Model
     use HasFactory;
 
     protected $fillable = [
-        'users_id',
+        'user_id',
         'products_id',
         'transactions_id',
         'quantity',
